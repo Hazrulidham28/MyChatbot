04a33a99b65fdfec38861b788919818feb467a56
Hazrul <2021816444@student.uitm.edu.my>
2024-12-15 19:37:26 +0800

    add other model

--- app.py
+++ app.py
@@ -6,19 +6,26 @@ import os
 # Load environment variables
 RUNPOD_API_KEY = os.getenv("RUNPOD_API_KEY")
 RUNPOD_ENDPOINT_ID = os.getenv("RUNPOD_ENDPOINT_ID")
+HF_ENDPOINT_ID =  os.getenv("HF_ENDPOINT_ID")
+GEMMA_API_KEY = os.getenv("GEMMA_API_KEY")
 
 app = Flask(__name__)
 
 
-client = OpenAI(
+runpod_client = OpenAI(
     api_key=RUNPOD_API_KEY,
     base_url=f"https://api.runpod.ai/v2/{RUNPOD_ENDPOINT_ID}/openai/v1",
 )
 
-def generate_response(query):
+gemma_client = OpenAI(
+	base_url=HF_ENDPOINT_ID,
+	api_key=GEMMA_API_KEY
+)
+
+def generate_response_runPod(query):
     # Combine context and query for better response generation
     prompt = query
-    response = client.chat.completions.create(
+    response = runpod_client.chat.completions.create(
         model = "TechxGenus/Meta-Llama-3-8B-Instruct-AWQ",
         messages=[
         {"role": "system", "content": "You are help full AI assistance that will answer everything about Malaysian or its culture. If the question ask anything else, do not answer"},
@@ -33,6 +40,28 @@ def generate_response(query):
 
     return response.choices[0].message.content
 
+def generate_response_gemma(query):
+    # Combine context and query for better response generation
+    prompt = f"""
+    You are a helpfull AI assistance that will help user answer question related to Malaysian. If user ask other than that, do not answer.
+    You also need to return the answer with the html tag, such as including the line break and etc...
+
+    query: {query}
+    """
+    response = gemma_client.chat.completions.create(
+        model = "google/gemma-2-27b-it",
+        messages=[
+        {
+            "role": "user",
+            "content":prompt,
+        },
+    ],
+    temperature=0.7,
+    max_tokens=1512,
+    )
+
+    return response.choices[0].message.content
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -40,8 +69,11 @@ def index():
 @app.route('/chat', methods=['POST'])
 def chat():
     user_query = request.form['query']
-    response = generate_response(user_query)
+    response = generate_response_gemma(user_query)
     return jsonify({"response": response})
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+# venv\Scripts\activate
